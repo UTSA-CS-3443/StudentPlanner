3c326e994d92d8463a66e04c39254e7bb1ee5c19
Stephanie <steph@10.40.38.92>
2021-11-15 16:42:46 -0600

    Linked all controllers to their respective fxml documents and created the main view with a button that will switch the user to the settings view

--- Planner/src/application/controller/MainController.java
+++ Planner/src/application/controller/MainController.java
@@ -1,5 +1,24 @@
 package application.controller;
 
-public class MainController {
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainController implements EventHandler<ActionEvent> {
+
+	@Override
+	public void handle(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/Settings.fxml"));
+			Main.stage.setScene(new Scene(root, 800, 800));
+			Main.stage.show();
+			} catch(Exception e) {
+			e.printStackTrace();
+			}
+		
+	}
 
 }
