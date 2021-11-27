fd51c37945a3e1a612b62c7d0fc09c49f334e029
Lucian Williams <Lucian.Williams@my.utsa.edu>
2021-11-27 15:20:37 -0600

    Fully finished priority view

--- Planner/src/application/controller/PriorityController.java
+++ Planner/src/application/controller/PriorityController.java
@@ -37,7 +37,7 @@ public class PriorityController implements EventHandler<MouseEvent>, Initializab
 		for (int i = 1; i < entries.size(); i++) {
 			for (int j = 0; j < entries.size() - i; j++) {
 				if (entries.get(j).getField(Settings.getPriorityOrdering()).getValue()
-						.compareTo(entries.get(j + 1).getField(Settings.getPriorityOrdering()).getValue()) < 0) {
+						.compareTo(entries.get(j + 1).getField(Settings.getPriorityOrdering()).getValue()) > 0) {
 					Entry temp = entries.get(j);
 					entries.set(j, entries.get(j + 1));
 					entries.set(j + 1, temp);
@@ -45,19 +45,28 @@ public class PriorityController implements EventHandler<MouseEvent>, Initializab
 			}
 		}
 		//fill in entry fields in view from model (link model to view)
-		if (entries.size() > 0) {
-			mainPane.getChildren().add(largeEntry(130, 30,
-					entries.get(0).getField(Settings.getPriorityTopLeft()).getValue(),
-					entries.get(0).getField(Settings.getPriorityTopRight()).getValue(),
-					entries.get(0).getName(),
-					entries.get(0).getField(Settings.getPriorityCenter()).getValue(),
-					entries.get(0).getField(Settings.getPriorityCenterBottom()).getValue(),
-					entries.get(0).getField(Settings.getPriorityBottomLeft()).getValue(),
-					entries.get(0).getField(Settings.getPriorityBottomRight()).getValue()));
+		mainPane.getChildren().clear();
+		for (int i = 0; i < 1 && i < entries.size(); i++) {
+			mainPane.getChildren().add(largeEntry(0, 30,
+					entries.get(i).getField(Settings.getPriorityTopLeft()).getValue(),
+					entries.get(i).getField(Settings.getPriorityTopRight()).getValue(),
+					entries.get(i).getName(),
+					entries.get(i).getField(Settings.getPriorityCenter()).getValue(),
+					entries.get(i).getField(Settings.getPriorityCenterBottom()).getValue(),
+					entries.get(i).getField(Settings.getPriorityBottomLeft()).getValue(),
+					entries.get(i).getField(Settings.getPriorityBottomRight()).getValue()));
 			
 		}
-		for (int i = 0; i < entries.size(); i++) {
+		for (int i = 1; i < 5 && i < entries.size(); i++) {
+			mainPane.getChildren().add(mediumEntry(330 + 165 * ((i - 1) % 2), 30 + 135 * ((i - 1) / 2),
+					entries.get(i).getField(Settings.getPriorityTopRight()).getValue(),
+					entries.get(i).getName()));
 			
+		}
+		for (int i = 5; i < 30 && i < entries.size(); i++) {
+			mainPane.getChildren().add(smallEntry(00 + 130 * ((i - 5) % 5), 300 + 98 * ((i - 5) / 5),
+					entries.get(i).getField(Settings.getPriorityTopRight()).getValue(),
+					entries.get(i).getName()));
 		}
 	}
 
@@ -123,6 +132,7 @@ public class PriorityController implements EventHandler<MouseEvent>, Initializab
 		Label topLeft = new Label();
 		topLeft.setFont(smallFont);
 		topLeft.setText(topLeftText);
+		topLeft.setWrapText(true);
 		topLeft.setPrefWidth(145);
 		topLeft.setMaxHeight(30);
 		topLeft.setLayoutX(5);
@@ -133,6 +143,7 @@ public class PriorityController implements EventHandler<MouseEvent>, Initializab
 		Label topRight = new Label();
 		topRight.setFont(smallFont);
 		topRight.setText(topRightText);
+		topRight.setWrapText(true);
 		topRight.setPrefWidth(145);
 		topRight.setMaxHeight(30);
 		topRight.setLayoutX(160);
@@ -143,53 +154,57 @@ public class PriorityController implements EventHandler<MouseEvent>, Initializab
 		Label centerTop = new Label();
 		centerTop.setFont(titleFont);
 		centerTop.setText(centerTopText);
+		centerTop.setWrapText(true);
 		centerTop.setPrefWidth(300);
 		centerTop.setMaxHeight(50);
 		centerTop.setLayoutX(5);
-		centerTop.setLayoutY(Math.max(topLeft.getHeight(), topRight.getHeight()) + 10);
+		centerTop.setLayoutY(40);
 		centerTop.setAlignment(Pos.TOP_CENTER);
 		centerTop.setTextAlignment(TextAlignment.CENTER);
 
 		Label center = new Label();
 		center.setFont(smallFont);
 		center.setText(centerText);
+		center.setWrapText(true);
 		center.setPrefWidth(300);
+		center.setMaxHeight(60);
 		center.setLayoutX(5);
-		center.setLayoutY(centerTop.getLayoutY() + centerTop.getHeight() + 5);
+		center.setLayoutY(95);
 		center.setAlignment(Pos.TOP_CENTER);
 		center.setTextAlignment(TextAlignment.CENTER);
 
 		Label centerBottom = new Label();
 		centerBottom.setFont(smallFont);
 		centerBottom.setText(centerBottomText);
+		centerBottom.setWrapText(true);
 		centerBottom.setPrefWidth(300);
 		centerBottom.setMaxHeight(30);
 		centerBottom.setLayoutX(5);
+		centerBottom.setLayoutY(160);
 		centerBottom.setAlignment(Pos.BOTTOM_CENTER);
 		centerBottom.setTextAlignment(TextAlignment.CENTER);
 
 		Label bottomLeft = new Label();
 		bottomLeft.setFont(smallFont);
 		bottomLeft.setText(bottomLeftText);
+		bottomLeft.setWrapText(true);
 		bottomLeft.setPrefWidth(145);
 		bottomLeft.setMaxHeight(30);
 		bottomLeft.setLayoutX(5);
-		bottomLeft.setLayoutY(235 - bottomLeft.getHeight());
+		bottomLeft.setLayoutY(205);
 		bottomLeft.setAlignment(Pos.BOTTOM_LEFT);
 		bottomLeft.setTextAlignment(TextAlignment.LEFT);
 
 		Label bottomRight = new Label();
 		bottomRight.setFont(smallFont);
 		bottomRight.setText(bottomRightText);
+		bottomRight.setWrapText(true);
 		bottomRight.setPrefWidth(145);
 		bottomRight.setMaxHeight(30);
 		bottomRight.setLayoutX(160);
-		bottomRight.setLayoutY(235 - bottomRight.getHeight());
+		bottomRight.setLayoutY(205);
 		bottomRight.setAlignment(Pos.BOTTOM_RIGHT);
 		bottomRight.setTextAlignment(TextAlignment.RIGHT);
-
-		centerBottom.setLayoutY(230 - Math.max(bottomLeft.getHeight(), bottomRight.getHeight()) - centerBottom.getHeight());
-		center.setMaxHeight(centerBottom.getLayoutY() - centerTop.getLayoutY() - centerTop.getHeight() - 10);
 
 		temp.getChildren().add(topLeft);
 		temp.getChildren().add(topRight);
@@ -198,6 +213,96 @@ public class PriorityController implements EventHandler<MouseEvent>, Initializab
 		temp.getChildren().add(centerBottom);
 		temp.getChildren().add(bottomLeft);
 		temp.getChildren().add(bottomRight);
+
+		return temp;
+	}
+	
+	private AnchorPane mediumEntry(int x, int y, String topText, String centerText) {
+
+		Font smallFont = Font.font("System", FontWeight.BOLD, FontPosture.REGULAR, 10.0);
+		Font titleFont = Font.font("System", FontWeight.BOLD, FontPosture.REGULAR, 16.0);
+
+		AnchorPane temp = new AnchorPane();
+		temp.setPrefWidth(145);
+		temp.setPrefHeight(105);
+		temp.setLayoutX(x);
+		temp.setLayoutY(y);
+
+		Rectangle rect = new Rectangle();
+		rect.setWidth(145);
+		rect.setHeight(105);
+		rect.setFill(Paint.valueOf("dddddd"));
+		temp.getChildren().add(rect);
+
+		Label top = new Label();
+		top.setFont(smallFont);
+		top.setText(topText);
+		top.setWrapText(true);
+		top.setPrefWidth(135);
+		top.setMaxHeight(30);
+		top.setLayoutX(5);
+		top.setLayoutY(5);
+		top.setAlignment(Pos.TOP_CENTER);
+		top.setTextAlignment(TextAlignment.CENTER);
+
+		Label center = new Label();
+		center.setFont(titleFont);
+		center.setText(centerText);
+		center.setWrapText(true);
+		center.setPrefWidth(135);
+		center.setLayoutX(5);
+		center.setLayoutY(40);
+		center.setAlignment(Pos.TOP_CENTER);
+		center.setTextAlignment(TextAlignment.CENTER);
+
+		center.setMaxHeight(100 - center.getLayoutY());
+
+		temp.getChildren().add(top);
+		temp.getChildren().add(center);
+
+		return temp;
+	}
+	private AnchorPane smallEntry(int x, int y, String topText, String centerText) {
+
+		Font smallFont = Font.font("System", FontWeight.BOLD, FontPosture.REGULAR, 10.0);
+
+		AnchorPane temp = new AnchorPane();
+		temp.setPrefWidth(120);
+		temp.setPrefHeight(78);
+		temp.setLayoutX(x);
+		temp.setLayoutY(y);
+
+		Rectangle rect = new Rectangle();
+		rect.setWidth(120);
+		rect.setHeight(78);
+		rect.setFill(Paint.valueOf("dddddd"));
+		temp.getChildren().add(rect);
+
+		Label top = new Label();
+		top.setFont(smallFont);
+		top.setText(topText);
+		top.setWrapText(true);
+		top.setPrefWidth(110);
+		top.setMaxHeight(30);
+		top.setLayoutX(5);
+		top.setLayoutY(5);
+		top.setAlignment(Pos.TOP_CENTER);
+		top.setTextAlignment(TextAlignment.CENTER);
+
+		Label center = new Label();
+		center.setFont(smallFont);
+		center.setText(centerText);
+		center.setWrapText(true);
+		center.setPrefWidth(110);
+		center.setLayoutX(5);
+		center.setLayoutY(40);
+		center.setAlignment(Pos.TOP_CENTER);
+		center.setTextAlignment(TextAlignment.CENTER);
+
+		center.setMaxHeight(73 - center.getLayoutY());
+
+		temp.getChildren().add(top);
+		temp.getChildren().add(center);
 
 		return temp;
 	}
