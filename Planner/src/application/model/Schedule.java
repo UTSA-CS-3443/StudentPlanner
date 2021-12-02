ba0cebbea9065da14def3784b210603e59a93b0d
Lucian Williams <Lucian.Williams@my.utsa.edu>
2021-12-02 15:33:34 -0600

    Javadoc for Schedule.java

--- Planner/src/application/model/Schedule.java
+++ Planner/src/application/model/Schedule.java
@@ -6,27 +6,55 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * The Schedule class represents a schedule containing Entries, and keeps track of active schedule files.
+ * 
+ * @author Stephanie Bassey, Lucian Williams, Azrah Al Rabeeah
+ */
 public class Schedule{
 	private static ArrayList<Entry> entries;
 	private static ArrayList<ToggleableFile> scheduleFiles;
 
+	/**
+	 * Initializes the static entries and scheduleFiles variables to new ArrayLists.
+	 */
 	public static void initialize() {
 		entries = new ArrayList<Entry>();
 		scheduleFiles = new ArrayList<ToggleableFile>();
 	}
 
+	/**
+	 * Returns entries.
+	 * 
+	 * @return entries.
+	 */
 	public static ArrayList<Entry> getEntries() {
 		return entries;
 	}
 
+	/**
+	 * Sets entries to the provided entries.
+	 * 
+	 * @param entries The new entries.
+	 */
 	public static void setEntries(ArrayList<Entry> entries) {
 		Schedule.entries = entries;
 	}
 
+	/**
+	 * Adds an entry to entries.
+	 * 
+	 * @param entry The new entry.
+	 */
 	public static void addEntry(Entry entry) {
 		entries.add(entry);
 	}
 
+	/**
+	 * Returns the Entry from entries with the given name or null if not found.
+	 * 
+	 * @return the matching Entry.
+	 */
 	public static Entry getEntry(String name) {
 		for (int i = 0; i < entries.size(); i++)
 			if (entries.get(i).getName().equals(name))
@@ -35,6 +63,11 @@ public class Schedule{
 		return null;
 	}
 
+	/**
+	 * Removes and returns the Entry from entries with the given name or null if not found.
+	 * 
+	 * @return the matching Entry.
+	 */
 	public static Entry removeEntry(String name) {
 		for (int i = 0; i < entries.size(); i++)
 			if (entries.get(i).getName().equals(name))
@@ -43,26 +76,54 @@ public class Schedule{
 		return null;
 	}
 
+	/**
+	 * Removes the provided Entry from entries and returns true if found, and false otherwise.
+	 * 
+	 * @return whether the Entry was found or not.
+	 */
 	public static boolean removeEntry(Entry entry) {
 		return entries.remove(entry);
 	}
 
+	/**
+	 * Clears entries.
+	 */
 	public static void clearEntries() {
 		entries = new ArrayList<Entry>();
 	}
 
+	/**
+	 * Returns scheduleFiles.
+	 * 
+	 * @return scheduleFiles.
+	 */
 	public static ArrayList<ToggleableFile> getScheduleFiles() {
 		return scheduleFiles;
 	}
 
+	/**
+	 * Sets scheduleFiles to the provided scheduleFiles.
+	 * 
+	 * @param scheduleFiles The new scheduleFiles.
+	 */
 	public static void setScheduleFiles(ArrayList<ToggleableFile> scheduleFiles) {
 		Schedule.scheduleFiles = scheduleFiles;
 	}
 
+	/**
+	 * Adds a scheduleFile to scheduleFiles.
+	 * 
+	 * @param scheduleFile The new scheduleFile.
+	 */
 	public static void addScheduleFile(ToggleableFile scheduleFile) {
 		scheduleFiles.add(scheduleFile);
 	}
 
+	/**
+	 * Returns the ToggleableFile from scheduleFiles with the given alias name or null if not found.
+	 * 
+	 * @return the matching ToggleableFile.
+	 */
 	public static ToggleableFile getScheduleFile(String aliasName) {
 		for (int i = 0; i < scheduleFiles.size(); i++)
 			if (scheduleFiles.get(i).getAliasName().equals(aliasName))
@@ -71,6 +132,11 @@ public class Schedule{
 		return null;
 	}
 
+	/**
+	 * Removes and returns the ToggleableFile from scheduleFiles with the given alias name or null if not found.
+	 * 
+	 * @return the matching ToggleableFile.
+	 */
 	public static ToggleableFile removeScheduleFile(String aliasName) {
 		for (int i = 0; i < scheduleFiles.size(); i++)
 			if (scheduleFiles.get(i).getAliasName().equals(aliasName))
@@ -79,14 +145,25 @@ public class Schedule{
 		return null;
 	}
 
+	/**
+	 * Removes the provided ToggleableFile from scheduleFiles and returns true if found, and false otherwise.
+	 * 
+	 * @return whether the ToggleableFile was found or not.
+	 */
 	public static boolean removeScheduleFile(ToggleableFile scheduleFile) {
 		return scheduleFiles.remove(scheduleFile);
 	}
 
+	/**
+	 * Clears scheduleFiles.
+	 */
 	public static void clearScheduleFiles() {
 		scheduleFiles = new ArrayList<ToggleableFile>();
 	}
 
+	/**
+	 * Loads entries from the files named by scheduleFiles into entries.
+	 */
 	public static void loadData() {
 		entries.clear();
 		for (int i = 0; i < scheduleFiles.size(); i++) {
@@ -124,6 +201,9 @@ public class Schedule{
 		}
 	}
 
+	/**
+	 * Saves entries to the files named by scheduleFiles from entries.
+	 */
 	public static void saveData() {
 		for (int i = 0; i < scheduleFiles.size(); i++) {
 			try {
